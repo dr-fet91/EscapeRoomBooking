606085fdbd17f9ec85095c6365c69ff905cfb8c4
dr-fet91 <dr.fet91@gmail.com>
2023-06-14 17:57:38 +0330

    fix: Fix the login problem

--- app/Http/Controllers/Auth/AuthenticateController.php
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Exception;
 
 class AuthenticateController extends Controller
 {
@@ -16,14 +17,24 @@ class AuthenticateController extends Controller
      */
     public function authenticate(Request $request)
     {
-        $credentials = $request->only('email', 'password');
+        try {
+            $request->validate([
+                'email' => ['required', 'max:256', 'email', 'exists:users,email'],
+                'password' => ['required'],
+            ]);
 
-        if (Auth::attempt($credentials)) {
-            // Authentication successful
-            return response()->json(['message' => 'Authenticated']);
-        } else {
-            // Authentication failed
-            return response()->json(['message' => 'Invalid credentials'], 401);
+            if (!Auth::attempt($request->only(['email', 'password']))) {
+                throw new Exception('Unauthorized! The username or password is incorrect', 401);
+            }
+
+            /** @var User $user */
+            $user = Auth::user();
+
+            return response([
+                'token' => $user->createToken("API TOKEN")->plainTextToken,
+            ], 200);
+        } catch (\Throwable $th) {
+            return $this->errorResponse($th);
         }
     }
 
