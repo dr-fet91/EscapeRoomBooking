cd36a318bc89c27bc8f8d21bdd9aa12e7b7ca26b
dr-fet91 <dr.fet91@gmail.com>
2023-06-14 14:36:38 +0330

    feat: create time_slots table & migration , controler , factory , etc
    create time_slots tables migration , policy, controller, request, factory, seeder

---
+++ app/Http/Controllers/TimeSlotController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TimeSlot;
+use App\Http\Requests\StoreTimeSlotRequest;
+use App\Http\Requests\UpdateTimeSlotRequest;
+
+class TimeSlotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTimeSlotRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TimeSlot $timeSlot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TimeSlot $timeSlot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTimeSlotRequest $request, TimeSlot $timeSlot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TimeSlot $timeSlot)
+    {
+        //
+    }
+}
