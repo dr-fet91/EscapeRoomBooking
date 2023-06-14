976dd92b3cf31fa2114f2288027717fb47fb7672
dr-fet91 <dr.fet91@gmail.com>
2023-06-14 14:49:17 +0330

    feat: create bookings table & migration , controler , factory , etc
    create bookings tables migration , policy, controller, request, factory, seeder

---
+++ database/migrations/2023_06_14_111016_create_bookings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('escape_room_id')->constrained('escape_rooms')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('time_slot_id')->constrained('time_slots')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->decimal('discount_percent', 5, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
